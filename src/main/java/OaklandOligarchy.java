17cde03a6ca50df447aed49e354f523cad69131b
Alexander Furst <ajf80@pitt.edu>
2017-06-02 15:59:36 -0400

    A rough depiction of the menu bar

--- src/main/java/OaklandOligarchy.java
+++ src/main/java/OaklandOligarchy.java
@@ -6,6 +6,9 @@ import java.util.ArrayList;
 public class OaklandOligarchy{
 	//main window for the application
 	JFrame window = new JFrame("OaklandOligarchy");
+	JButton newGame = new JButton("New Game");
+	JButton endGame = new JButton("End Game");
+
 	//list of all the players in this game
 	ArrayList<Player> all_players = new ArrayList<>();
 	//manual creation of players for skeleton purposes
@@ -13,7 +16,7 @@ public class OaklandOligarchy{
 	Player p2 = new Player("player2", 12000);
 	Player p3 = new Player("player3HasAReallyLongNameSoLetsSeeWhadfasdf", 800);
 	public static void main(String[] args){
-		new OaklandOligarchy();	
+		new OaklandOligarchy();
 	}//end of main method
 
 	//constructor will start the gui
@@ -25,7 +28,24 @@ public class OaklandOligarchy{
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setSize(1000,1000);
 
-		
+		//add menu panel accross the top of the game UI
+		JPanel menu = new JPanel();
+		menu.setPreferredSize(new Dimension(1000, 100));
+		window.add(menu, BorderLayout.NORTH);
+		menu.setLayout(new GridLayout(0, 6));
+
+		//set game title in top left
+		JLabel title = new JLabel("<html>Oakland<br>Oligarchy</html>");
+		title.setFont(new Font("Times", Font.PLAIN, 30));
+		menu.add(title, 0, 0);
+
+		//set font for buttons in menu panel
+		newGame.setFont(new Font("Courier", Font.PLAIN, 20));
+		menu.add(newGame, 0, 1);
+
+		endGame.setFont(new Font("Courier", Font.PLAIN, 20));
+		menu.add(endGame, 0, 2);
+
 		//add info to the left side of the frame
 		InfoPanel info = new InfoPanel(all_players);
 		window.add(info, BorderLayout.WEST);
@@ -37,20 +57,3 @@ public class OaklandOligarchy{
 
 
 }//end of class OaklandOligarchy
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
