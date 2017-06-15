f32f772a1961d4ef61803924ef63c2bfd1c1262e
NathanAckerman <nga5@pitt.edu>
2017-06-15 09:34:58 -0400

    added javadoc, magic number as variable, and formatted text to left justification

--- src/main/java/InfoPanel.java
+++ src/main/java/InfoPanel.java
@@ -15,21 +15,27 @@ public class InfoPanel extends JPanel{
 
 		infoLabel.setHorizontalTextPosition(SwingConstants.LEFT);
 		infoLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		this.setLayout(new FlowLayout(FlowLayout.LEFT));
 		this.add(infoLabel); 
 		//set formatting for the panel
 		this.setPreferredSize(new Dimension(200,1000));
 		this.setBorder(BorderFactory.createLineBorder(Color.black));
 	}//end of constructor
 
-	//TODO more formatting on text alignment	
-	//this method will refresh the info
+	/*This method will refresh the game info in the InfoPanel object
+	 *@param all_players the arraylist containing all of the players in the game
+	 *@param tiles       the tile object which has an array of all the tiles
+	 *@return none
+	 */
 	public void refresh(ArrayList<Player> all_players, ImplementTiles tiles){
 		String str = new String("");//this string will be used to build str for info JLabel
 		
+		final int NUM_TILES = 36;//make final int to avoid magic number, this is num tiles in tile array
+
 		str = str + "<html><br>";//begin the opening html tag
 		int player_number = 0;
 		for(Player p : all_players){//loop through all the players and get their info
-			str = str + "<b><u>" + p.getName() + "</u></b> <br>";//get player name
+			str = str + "<div style='font-size:16'><b><u>" + p.getName() + "</u></b></div> <br>";//get player name
 			String color = "Color: ";
 			if(player_number==0){
 				color = color + "Red";
@@ -47,7 +53,7 @@ public class InfoPanel extends JPanel{
 			str = str + "Properties: <br>";//now list all properties owned
 			
 			int num_props = 0;//keep track of number of owned props
-			for(int i = 0; i < 36; i++){//loop through all props and find ones that this player owns
+			for(int i = 0; i < NUM_TILES; i++){//loop through all props and find ones that this player owns
 				if(tiles.getTile(i).getOwner() == p){
 					str = str + tiles.getTile(i).getTileName() + "<br>";
 					num_props++;
