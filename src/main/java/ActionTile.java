767e2f23c334fe9cd8b1fef03c518bfa2e8e67e5
Alexander Furst <ajf80@pitt.edu>
2017-06-15 10:52:47 -0400

    ex7

--- src/main/java/ActionTile.java
+++ src/main/java/ActionTile.java
@@ -2,7 +2,7 @@
 //representing an action tile on the gameboard
 //actions can be positive or negative outcomes (hence conflicting attributes types)
 public class ActionTile extends Tile{
-
+	//represent the output on the board in tileInfo String
     private String tileInfo;
 
     ActionTile(String description){
