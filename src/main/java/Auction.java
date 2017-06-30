74f7c997ebbadb64eaf3df6e78df094c4fed82de
NathanAckerman <nga5@pitt.edu>
2017-06-29 23:40:20 -0400

    added Auction.java

---
+++ src/main/java/Auction.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class Auction extends JDialog{
+
+	Auction(ArrayList<Player> other_players, Player currentPlayer, OaklandOligarchy game){
+
+	}//end of constructor
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//end of class Auction
