50d5a7c7ca4c944eb7d727de27f2e33e371c0688
NathanAckerman <nga5@pitt.edu>
2017-07-08 09:48:09 -0400

    fixed tests for new constructors for saving/loading

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -5,19 +5,19 @@ public class PlayerTest {
 
 	@Test
 	public void testPlayerName() {
-		Player p = new Player("pname", 3000, 0);
+		Player p = new Player("pname", 3000, 0, false);
 		assertEquals(p.getName(), "PNAME");
 	}
 
 	@Test
 	public void testPlayerMoney() {
-		Player p = new Player("pname", 9001, 0);
+		Player p = new Player("pname", 9001, 0, false);
 		assertEquals(p.getMoney(), 9001);
 		p.setMoney(9002);
 		assertEquals(p.getMoney(), 9002);
 		p.setMoney(-10);
 		assertEquals(p.getMoney(), 0);
-		Player p2 = new Player("p2name", -20, 0);
+		Player p2 = new Player("p2name", -20, 0, false);
 		assertEquals(p2.getMoney(), 0);
 	}
 
